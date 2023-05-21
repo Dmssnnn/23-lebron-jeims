31fd488a6ba93e579c0cb61e24920659cf4ecb18
Dmssnnn <129981482+Dmssnnn@users.noreply.github.com>
2023-05-22 02:25:12 +0400

    add index,css, js

---
+++ script.js
@@ -0,0 +1,40 @@
+// let boolean = true;
+// let string = 50;
+let srulwlovani = true;
+let asaki = "50";
+let wona = 95; 
+let saxeli
+let tve = 'octobe';
+
+
+let a = 20;
+let b = 50;
+let c = a + b; 
+console.log (c); 
+
+a = 100 ;
+b = 20 ;
+c = a - b; 
+console.log (c);
+
+a = 200 ; 
+b = 100 ; 
+c = 200 / 100;
+console.log(c);
+
+a = 500; 
+b = 5 ;
+c = 500 * 5 ; 
+console.log(c);
+
+let FirstName = 'dimitri'
+let lastName = 'alavidze'
+let age = '80';
+let height = '183'
+console.log(`my name is ${FirstName} ${lastName}. i am ${age} years old. my height is like ${height}cm. madloba, naxvamdis. `  );
+console.log(" my name is " + FirstName + " " + " and my last name is " + lastName )
+
+
+let j = 7;
+let k = 5;
+console.log(j * k);
